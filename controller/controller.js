52bc74a6e3a4def64a35f2bdb5529dfa1ba7a6be
Andrii Petrashenko <petrashenko@gmail.com>
2022-05-29 22:37:00 +0300

    comments

--- controller/controller.js
+++ controller/controller.js
@@ -3,14 +3,14 @@ import TextItemsView from "../view/textItemsView.js";
 
 export default class Controller {
     constructor() {
-        // link to html element where the list is stored
+        // link to html element where the list is displayed
         const appElement = document.getElementById("app");
         // model
         this.textItems = new TextItems();
         // view
         this.textItemsView = new TextItemsView(appElement, this.textItems);
 
-        // html elements' on click handlers
+        // html element's on-click handlers
         document.getElementById("makeTextLowerCase").addEventListener( "click", (e)=> {this.makeAllLowerCase()});
     }
 
